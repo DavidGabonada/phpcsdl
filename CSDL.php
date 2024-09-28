f3a3ee8545483831a7151438ac41079988f92a02
Nicht <gabonadadavidjosh@gmail.com>
2024-09-28 12:36:34 +0800

    first commit

---
+++ CSDL.php
@@ -0,0 +1,444 @@
+<?php
+include "headers.php";
+
+class User
+{
+
+  function addadministrator($json)
+  {
+    include "connection.php";
+    $json = json_decode($json, true);
+    $sql = "INSERT INTO tbl_admin(adm_employee_id, adm_first_name, adm_last_name, adm_password, adm_email)
+    VALUES(1, :adm_first_name, :adm_last_name, :adm_password, :adm_email)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam("adm_first_name", $json["adm_first_name"]);
+    $stmt->bindParam("adm_last_name", $json["adm_last_name"]);
+    $stmt->bindParam("adm_password", $json["adm_password"]);
+    $stmt->bindParam("adm_email", $json["adm_email"]);
+
+    $stmt->execute();
+    return $stmt->rowCount() > 0 ? 1 : 0;
+  }
+  function addDepartment($json)
+  {
+    include "connection.php";
+    $json = json_decode($json, true);
+    $sql = "INSERT INTO tbl_departments(dept_name)
+    VALUES(:dept_name)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam("dept_name", $json["dept_name"]);
+
+    $stmt->execute();
+    return $stmt->rowCount() > 0 ? 1 : 0;
+  }
+
+  function addSchoolyear($json)
+  {
+    include "connection.php";
+    $json = json_decode($json, true);
+    $sql = "INSERT INTO tbl_sy(sy_name, sy_status)
+    VALUES(:sy_name, :sy_status)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam("sy_name", $json["sy_name"]);
+    $stmt->bindParam("sy_status", $json["sy_status"]);
+    $stmt->execute();
+    return $stmt->rowCount() > 0 ? 1 : 0;
+  }
+
+  function addCourse($json)
+  {
+    include "connection.php";
+    $json = json_decode($json, true);
+    $sql = "INSERT INTO tbl_course(crs_name, crs_dept_id)
+    VALUES(:crs_name, :crs_dept_id)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam("crs_name", $json["crs_name"]);
+    $stmt->bindParam("crs_dept_id", $json["crs_dept_id"]);
+    $stmt->execute();
+    return $stmt->rowCount() > 0 ? 1 : 0;
+  }
+
+  function addScholarship_type($json)
+  {
+    include "connection.php";
+    $json = json_decode($json, true);
+    $sql = "INSERT INTO tbl_scholarship_type(type_name)
+    VALUES(:type_name)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam("type_name", $json["type_name"]);
+
+    $stmt->execute();
+    return $stmt->rowCount() > 0 ? 1 : 0;
+  }
+
+  function addScholar($json)
+  {
+    // {"stud_firstname":"joe","stud_lastname":"rogan","stud_year_level":"1st Year","stud_course_id":1,"scholarship_type_id":1,"stud_scholarship_sub_id":2}
+    include "connection.php";
+    $json = json_decode($json, true);
+    $sql = "INSERT INTO tbl_scholars(stud_school_id, stud_first_name, stud_last_name, stud_year_level, stud_scholarship_type_id, stud_scholarship_sub_type_id, stud_course_id) 
+    VALUES(:stud_school_id, :stud_firstname, :stud_lastname, :stud_year_level, :scholarship_type_id, :stud_scholarship_sub_id, :stud_course_id)";
+    $stmt = $conn->prepare($sql);
+
+
+    $stmt->bindParam("stud_firstname", $json["stud_firstname"]);
+    $stmt->bindParam("stud_lastname", $json["stud_lastname"]);
+    $stmt->bindParam("stud_year_level", $json["stud_year_level"]);
+    $stmt->bindParam("scholarship_type_id", $json["scholarship_type_id"]);
+    $stmt->bindParam("stud_scholarship_sub_id", $json["stud_scholarship_sub_id"]);
+    $stmt->bindParam("stud_course_id", $json["stud_course_id"]);
+    $stmt->bindParam("stud_school_id", $json["stud_school_id"]);
+
+    $stmt->execute();
+    return $stmt->rowCount() > 0 ? 1 : 0;
+  }
+
+  function addSchoolar_sub_type($json)
+
+  {
+    include "connection.php";
+    $json = json_decode($json, true);
+    $sql = "INSERT INTO tbl_departments(stype_type_id, stype_name, stype_max_hours)
+    VALUES(:stype_type_id, :stype_name, :stype_max_hours)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam("stype_type_id", $json["stype_type_id"]);
+    $stmt->bindParam("stype_name", $json["stype_name"]);
+    $stmt->bindParam("stype_max_hours", $json["stype_max_hours"]);
+
+    $stmt->execute();
+    return $stmt->rowCount() > 0 ? 1 : 0;
+  }
+
+  function addOfficeMaster($json)
+  {
+    include "connection.php";
+    $json = json_decode($json, true);
+    $sql = "INSERT INTO tbl_office_master(off_name, off_subject_code, off_descriptive_title, off_section, off_room, off_type_id, off_timeIn, off_timeOut, off_dayRemote, off_remoteTimeIn, off_remoteTimeOut)
+    VALUES(:off_name, :off_subject_code, :off_descriptive_title, :off_section, :off_room, :off_type_id, :off_timeIn, :off_timeOut :off_dayRemote, :off_remoteTimeIn, :off_remoteTimeOut)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam("off_name", $json["off_name"]);
+    $stmt->bindParam("off_subject_code", $json["off_subject_code"]);
+    $stmt->bindParam("off_descriptive_title", $json["off_descriptive_title"]);
+    $stmt->bindParam("off_section", $json["off_section"]);
+    $stmt->bindParam("off_room", $json["off_room"]);
+    $stmt->bindParam("off_type_id", $json["off_type_id"]);
+    $stmt->bindParam("off_timeIn", $json["off_timeIn"]);
+    $stmt->bindParam("off_timeOut", $json["off_timeOut"]);
+    $stmt->bindParam("off_dayRemote", $json["off_dayRemote"]);
+    $stmt->bindParam("off_remoteTimeIn", $json["off_remoteTimeIn"]);
+    $stmt->bindParam("off_remoteTimeOut", $json["off_remoteTimeOut"]);
+
+    $stmt->execute();
+    return $stmt->rowCount() > 0 ? 1 : 0;
+  }
+
+
+  function getscholarship_type()
+  {
+    include "connection.php";
+    $sql = "SELECT * FROM tbl_scholarship_type";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    return $stmt->rowCount() > 0 ? $stmt->fetchAll(PDO::FETCH_ASSOC) : 0;
+  }
+
+  function getcourse()
+  {
+    include "connection.php";
+    $sql = "SELECT * FROM tbl_course";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    return $stmt->rowCount() > 0 ? $stmt->fetchAll(PDO::FETCH_ASSOC) : 0;
+  }
+
+  function getscholarship_type_list()
+  {
+    include "connection.php";
+    $sql = "SELECT * FROM tbl_scholarship_type";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    return $stmt->rowCount() > 0 ? $stmt->fetchAll(PDO::FETCH_ASSOC) : 0;
+  }
+  function getSubType()
+  {
+    include "connection.php";
+    $sql = "SELECT * FROM tbl_scholarship_sub_type";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    return $stmt->rowCount() > 0 ? $stmt->fetchAll(PDO::FETCH_ASSOC) : 0;
+  }
+
+  function getschoolyear()
+  {
+    include "connection.php";
+    $sql = "SELECT * FROM tbl_sy";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    return $stmt->rowCount() > 0 ? $stmt->fetchAll(PDO::FETCH_ASSOC) : [];
+  }
+
+  function getScholar()
+  {
+    include "connection.php";
+    $sql = "SELECT * FROM tbl_scholars";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    return $stmt->rowCount() > 0 ? $stmt->fetchAll(PDO::FETCH_ASSOC) : [];
+  }
+
+  function getSchoolYearLevel()
+  {
+    include "connection.php";
+    $sql = "SELECT * FROM tbl_school_year_level";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    return $stmt->rowCount() > 0 ? $stmt->fetchAll(PDO::FETCH_ASSOC) : [];
+  }
+
+  function getAddScholarDropDown()
+  {
+    include "connection.php";
+    $returnValue = [];
+    $returnValue["yearLevel"] = $this->getSchoolYearLevel();
+    $returnValue["course"] = $this->getcourse();
+    $returnValue["scholarshipType"] = $this->getscholarship_type();
+    $returnValue["scholarshipSub"] = $this->getSubType();
+
+    return json_encode($returnValue);
+  }
+
+  function getadminList()
+  {
+    include "connection.php";
+    $sql = "SELECT * FROM tbl_admin";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    return $stmt->rowCount() > 0 ? $stmt->fetchAll(PDO::FETCH_ASSOC) : [];
+  }
+
+  function getCourseList()
+  {
+    include "connection.php";
+    $sql = "SELECT * FROM tbl_course";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    return $stmt->rowCount() > 0 ? $stmt->fetchAll(PDO::FETCH_ASSOC) : [];
+  }
+
+  function getDepartment()
+  {
+    include "connection.php";
+    $sql = "SELECT * FROM tbl_departments";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    return $stmt->rowCount() > 0 ? $stmt->fetchAll(PDO::FETCH_ASSOC) : [];
+  }
+  function getScholarTypeList()
+  {
+    include "connection.php";
+    $sql = "SELECT * FROM tbl_scholarship_type";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    return $stmt->rowCount() > 0 ? $stmt->fetchAll(PDO::FETCH_ASSOC) : [];
+  }
+
+  function getSchoolYearList()
+  {
+    include "connection.php";
+    $sql = "SELECT * FROM tbl_sy";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    return $stmt->rowCount() > 0 ? $stmt->fetchAll(PDO::FETCH_ASSOC) : [];
+  }
+
+  function getOfficeMaster()
+  {
+    include "connection.php";
+    $sql = "SELECT * FROM tbl_office_master";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    return $stmt->rowCount() > 0 ? $stmt->fetchAll(PDO::FETCH_ASSOC) : [];
+  }
+
+  function getSupervisor()
+  {
+    include "connection.php";
+    $sql = "SELECT * FROM tbl_office_supervisors";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    return $stmt->rowCount() > 0 ? $stmt->fetchAll(PDO::FETCH_ASSOC) : [];
+  }
+  function getScholarshipSubType()
+  {
+    include "connection.php";
+    $sql = "SELECT * FROM tbl_scholarship_sub_type";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    return $stmt->rowCount() > 0 ? $stmt->fetchAll(PDO::FETCH_ASSOC) : [];
+  }
+
+  function getAllList()
+  {
+    include "connection.php";
+    $returnValue = [];
+    $returnValue["adminList"] = $this->getadminList();
+    $returnValue["departmentList"] = $this->getDepartment();
+    $returnValue["schoolyearlist"] = $this->getschoolyear();
+    $returnValue["courselist"] = $this->getCourseList();
+    $returnValue["scholarshiptypelist"] = $this->getscholarship_type_list();
+    $returnValue["officeMasterlist"] = $this->getOfficeMaster();
+    $returnValue["scholarlist"] = $this->getScholar();
+    $returnValue["supervisorlist"] = $this->getSupervisor();
+    $returnValue["scholarsubtype"] = $this->getScholarshipSubType();
+    return json_encode($returnValue);
+  }
+
+  function updateAdmin($json)
+  {
+    //{"adm_employee_id":1, "adm_first_name":"bea","adm_last_name":"macario", "adm_password":"143llove", "adm_email": "beamacario@gmail.com", "adm_id":1}
+    include "connection.php";
+    $data = json_decode($json, true);
+    $sql = "UPDATE tbl_admin SET adm_employee_id = :adm_employee_id, adm_first_name = :adm_first_name, adm_last_name = :adm_last_name, adm_password = :adm_password, adm_email = :adm_email WHERE adm_id = :adm_id";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(":adm_employee_id", $data["adm_employee_id"]);
+    $stmt->bindParam(":adm_first_name", $data["adm_first_name"]);
+    $stmt->bindParam(":adm_last_name", $data["adm_last_name"]);
+    $stmt->bindParam(":adm_password", $data["adm_password"]);
+    $stmt->bindParam(":adm_email", $data["adm_email"]);
+    $stmt->bindParam(":adm_id", $data["adm_id"]);
+    $stmt->execute();
+    return $stmt->rowCount() > 0 ? 1 : 0;
+  }
+
+  function updateDerpartment($json)
+  {
+    include "connection.php";
+    $data = json_decode($json, true);
+    $sql = "UPDATE tbl_departments SET dep_name = :dep_name WHERE dep_id = :dep_id";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(":dep_name", $data["dep_name"]);
+    $stmt->bindParam(":dep_id", $data["dep_id"]);
+    $stmt->execute();
+    return $stmt->rowCount() > 0 ? 1 : 0;
+  }
+}
+
+
+
+
+$json = isset($_POST["json"]) ? $_POST["json"] : "0";
+$operation = isset($_POST["operation"]) ? $_POST["operation"] : "0";
+
+$user = new User();
+
+switch ($operation) {
+
+  case "addDepartment":
+    echo $user->addDepartment($json);
+    break;
+
+  case "addSchoolyear":
+    echo $user->addSchoolyear($json);
+    break;
+
+  case "addCourse":
+    echo $user->addCourse($json);
+    break;
+
+  case "addScholarship_type":
+    echo $user->addScholarship_type($json);
+    break;
+
+  case "addScholar":
+    echo $user->addScholar($json);
+    break;
+
+  case "addSchoolar_sub_type":
+    echo $user->addSchoolar_sub_type($json);
+    break;
+
+  case "addOfficeMaster":
+    echo $user->addOfficeMaster($json);
+    break;
+
+  case "addScholarship_type":
+    echo $user->addScholarship_type($json);
+    break;
+
+  case "addadministrator":
+    echo $user->addadministrator($json);
+    break;
+
+  case "getAddScholarDropDown":
+    echo $user->getAddScholarDropDown();
+    break;
+
+  case "getscholarship_type":
+    echo $user->getscholarship_type();
+    break;
+
+  case "getschoolyear":
+    echo $user->getschoolyear();
+    break;
+
+  case "getSupervisor":
+    echo $user->getSupervisor();
+    break;
+
+  case "getScholarshipSubType":
+    echo $user->getScholarshipSubType();
+    break;
+
+  case "getAdminList":
+    echo $user->getadminList();
+    break;
+
+  case "getCourseList":
+    echo $user->getCourseList();
+    break;
+
+  case "getSchoolYearList":
+    echo $user->getSchoolYearList();
+    break;
+
+  case "getDepartment":
+    echo $user->getDepartment();
+    break;
+
+  case "getcourse":
+    echo $user->getcourse();
+    break;
+  case "getSubType":
+    echo $user->getSubType();
+    break;
+
+  case "getSchoolYearLevel":
+    echo $user->getSchoolYearLevel();
+    break;
+
+  case "getDepartment":
+    echo $user->getDepartment();
+    break;
+
+  case "getOfficeMaster":
+    echo $user->getOfficeMaster();
+    break;
+
+  case "getScholarTypeList":
+    echo $user->getScholarTypeList();
+    break;
+
+  case "getAllList":
+    echo $user->getAllList();
+    break;
+
+  case "updateAdmin":
+    echo $user->updateAdmin($json);
+    break;
+
+
+
+  default:
+    echo "WALAY " . $operation . " NGA OPERATION SA UBOS HAHHAHA BOBO";
+    break;
+}
